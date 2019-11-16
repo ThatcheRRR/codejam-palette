ef3b515c610e0e901446ea88914ca34259d1fcb2
ThatcheRRR <pavelosipov777@mail.ru>
2019-11-17 02:22:33 +0300

    refactor: rewrite code accordit to eslint airbnb style

--- palette/app.js
+++ palette/app.js
@@ -1,209 +1,211 @@
-const canvas = document.querySelector('canvas'),
-	pencil = document.querySelector('.pencil'),
-	bucket = document.querySelector('.bucket'),
-	choose = document.querySelector('.choose-color'),
-	tool = document.querySelectorAll('.tool'),
-	size = 4,
-	pencilCode = 80,
-	bucketCode = 66,
-    chooseCode = 67,
-    maxFieldSize = 512,
-	redColor = '#F74141',
-    blueColor = '#00BCD4',
-    ctx = canvas.getContext('2d'),
-    red = document.querySelector('.red'),
-    blue = document.querySelector('.blue'),
-    previous = document.querySelector('.previous-color'),
-    color = document.querySelector('input'),
-    scale = Math.round(maxFieldSize / size),
-    tools = ['bucket', 'colorPicker', 'pencil'];
-      
-let	pickerActive = false,
-	currentColor = '#FFC107',
-	previousColor = '#FFEB3B',
-    activeTool = 'pencil',
-    matrix = [];
+const canvas = document.querySelector('canvas');
+const pencil = document.querySelector('.pencil');
+const bucket = document.querySelector('.bucket');
+const choose = document.querySelector('.choose-color');
+const tool = document.querySelectorAll('.tool');
+const size = 4;
+const pencilCode = 80;
+const bucketCode = 66;
+const chooseCode = 67;
+const maxFieldSize = 512;
+const redColor = '#F74141';
+const blueColor = '#00BCD4';
+const ctx = canvas.getContext('2d');
+const red = document.querySelector('.red');
+const blue = document.querySelector('.blue');
+const previous = document.querySelector('.previous-color');
+const color = document.querySelector('input');
+const sc = Math.round(maxFieldSize / size);
+const tools = ['bucket', 'colorPicker', 'pencil'];
+
+let pickerActive = false;
+let currentColor = '#FFC107';
+let previousColor = '#FFEB3B';
+let activeTool = 'pencil';
+let matrix = [];
 
 if (localStorage.getItem('fieldStatus') === null) {
-    matrix = [
-        ['#fff', '#fff','#fff','#fff'],
-        ['#fff', '#fff','#fff','#fff'],
-        ['#fff', '#fff','#fff','#fff'],
-        ['#fff', '#fff','#fff','#fff']
-    ];
+  matrix = [
+    ['#fff', '#fff', '#fff', '#fff'],
+    ['#fff', '#fff', '#fff', '#fff'],
+    ['#fff', '#fff', '#fff', '#fff'],
+    ['#fff', '#fff', '#fff', '#fff'],
+  ];
 } else if (localStorage.getItem('fieldStatus') !== null) {
-    let t = [], counter = 0;
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            t.push(localStorage.getItem('fieldStatus').split(',')[counter]);
-            counter++;
-        }
-        matrix.push(t);
-        t = [];
+  let t = []; let
+    counter = 0;
+  for (let i = 0; i < size; i += 1) {
+    for (let j = 0; j < size; j += 1) {
+      t.push(localStorage.getItem('fieldStatus').split(',')[counter]);
+      counter += 1;
     }
+    matrix.push(t);
+    t = [];
+  }
 }
 
 pencil.classList.add('active-tool');
 
-for (let i = 0; i < tool.length; i++) {
-    tool[i].addEventListener('mousedown', selectTool);
+function canvasInit() {
+  canvas.width = 512;
+  canvas.height = 512;
+  const blockWidth = matrix[0].length;
+  const blockHeight = matrix.length;
+  for (let row = 0; row < blockWidth; row += 1) {
+    for (let col = 0; col < blockHeight; col += 1) {
+      ctx.fillStyle = matrix[row][col];
+      ctx.fillRect(col * sc, row * sc, sc, sc);
+    }
+  }
+}
+
+canvasInit();
+
+function changeColors() {
+  if (previousColor === currentColor) return;
+  document.querySelector('.cur-color').style.backgroundColor = currentColor;
+  document.querySelector('.prev-color').style.backgroundColor = previousColor;
+}
+
+function rgbConvert(r, g, b) {
+  let redConvert = r.toString(16);
+  let greenConvert = g.toString(16);
+  let blueConvert = b.toString(16);
+  if (redConvert.length === 1) redConvert = `0${redConvert}`;
+  if (greenConvert.length === 1) greenConvert = `0${greenConvert}`;
+  if (blueConvert.length === 1) blueConvert = `0${blueConvert}`;
+  return `#${redConvert}${greenConvert}${blueConvert}`;
+}
+
+const kek = document.querySelector('.colors');
+
+function drawing(e) {
+  if (activeTool === 'pencil') {
+    ctx.fillStyle = currentColor;
+    ctx.fillRect(sc * Math.floor(e.offsetX / sc), sc * Math.floor(e.offsetY / sc), sc, sc);
+    canvas.onmousemove = (k) => {
+      ctx.fillRect(sc * Math.floor(k.offsetX / sc), sc * Math.floor(k.offsetY / sc), sc, sc);
+    };
+    canvas.onmouseup = () => {
+      canvas.onmousemove = null;
+    };
+    canvas.onmouseout = () => {
+      canvas.onmousemove = null;
+    };
+  } else if (activeTool === 'bucket') {
+    canvas.getContext('2d').fillStyle = currentColor;
+    canvas.getContext('2d').fillRect(0, 0, maxFieldSize, maxFieldSize);
+  }
+}
+
+function colorPicker(e) {
+  if (pickerActive === true && e.target === canvas) {
+    const r = ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data[0];
+    const g = ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data[1];
+    const b = ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data[2];
+    previousColor = currentColor;
+    currentColor = rgbConvert(r, g, b);
+    if (previousColor === currentColor) {
+      return;
+    }
+    changeColors();
+  } else if (e.currentTarget === tool[1]) {
+    pickerActive = true;
+    activeTool = 'colorPicker';
+  }
+}
+
+function changeCurrentColor(e) {
+  if (e.target === previous || e.target === previous.children[1]) {
+    const temp = currentColor;
+    currentColor = previousColor;
+    previousColor = temp;
+    changeColors();
+  }
+  if (e.target === red || e.target === red.children[1]) {
+    previousColor = currentColor;
+    currentColor = redColor;
+    changeColors();
+  }
+  if (e.target === blue || e.target === blue.children[1]) {
+    previousColor = currentColor;
+    currentColor = blueColor;
+    changeColors();
+  }
+}
+
+function save() {
+  const arr = [];
+  for (let i = 0; i < size; i += 1) {
+    for (let j = 0; j < size; j += 1) {
+      const r = ctx.getImageData(sc * j + 1, sc * i + 1, 1, 1).data[0];
+      const g = ctx.getImageData(sc * j + 1, sc * i + 1, 1, 1).data[1];
+      const b = ctx.getImageData(sc * j + 1, sc * i + 1, 1, 1).data[2];
+      arr.push(rgbConvert(r, g, b));
+    }
+  }
+  localStorage.setItem('fieldStatus', arr);
+}
+
+function selectTool(e) {
+  for (let i = 0; i < tool.length; i += 1) {
+    tool[i].classList.remove('active-tool');
+    if (e.currentTarget === tool[i]) {
+      tool[i].classList.add('active-tool');
+      activeTool = tools[i];
+      if (activeTool !== 'colorPicker') {
+        pickerActive = false;
+      }
+    }
+  }
+}
+
+function selectCurrentColor() {
+  previousColor = currentColor;
+  currentColor = color.value;
+  changeColors();
+}
+
+function binds(event) {
+  switch (event.keyCode) {
+    case pencilCode:
+      for (let i = 0; i < tool.length; i += 1) {
+        tool[i].classList.remove('active-tool');
+      }
+      pencil.classList.add('active-tool');
+      activeTool = 'pencil';
+      pickerActive = false;
+      break;
+    case bucketCode:
+      for (let i = 0; i < tool.length; i += 1) {
+        tool[i].classList.remove('active-tool');
+      }
+      bucket.classList.add('active-tool');
+      activeTool = 'bucket';
+      pickerActive = false;
+      break;
+    case chooseCode:
+      for (let i = 0; i < tool.length; i += 1) {
+        tool[i].classList.remove('active-tool');
+      }
+      choose.classList.add('active-tool');
+      activeTool = 'colorPicker';
+      pickerActive = true;
+      colorPicker(event);
+      break;
+    default:
+      break;
+  }
+}
+
+for (let i = 0; i < tool.length; i += 1) {
+  tool[i].addEventListener('mousedown', selectTool);
 }
 
 canvas.addEventListener('mousedown', drawing);
 choose.addEventListener('mousedown', colorPicker);
 canvas.addEventListener('mousedown', colorPicker);
-document.addEventListener('mousedown', changeCurrentColor);
+kek.addEventListener('mousedown', changeCurrentColor, true);
 choose.addEventListener('input', selectCurrentColor);
 document.addEventListener('keyup', binds);
 window.addEventListener('beforeunload', save);
-
-function canvasInit() {
-	canvas.width = 512;
-	canvas.height = 512;
-	let blockWidth = matrix[0].length,
-        blockHeight = matrix.length;
-    for (let row = 0; row < blockWidth; row++) {
-        for (let col = 0; col < blockHeight; col++) {
-            ctx.fillStyle = matrix[row][col];
-            ctx.fillRect(col * scale, row * scale, scale, scale);
-        }
-    }
-}
-
-canvasInit();
-
-function drawing(event) {
-    if (activeTool === 'pencil') {
-        ctx.fillStyle = currentColor;
-        ctx.fillRect(128 * Math.floor(event.offsetX / 128), 128 * Math.floor(event.offsetY / 128), 128, 128);
-        canvas.onmousemove = function(event) {
-            ctx.fillRect(128 * Math.floor(event.offsetX / 128), 128 * Math.floor(event.offsetY / 128), 128, 128);
-        }
-        canvas.onmouseup = function() {
-            canvas.onmousemove = null;
-        }
-        canvas.onmouseout = function() {
-            canvas.onmousemove = null;
-        }
-    } else if (activeTool === 'bucket') {
-        canvas.getContext('2d').fillStyle = currentColor;
-        canvas.getContext('2d').fillRect(0, 0, 512, 512);
-    }
-}
-
-function colorPicker(event) {
-    if (pickerActive === true  && event.target === canvas) {
-        let r = ctx.getImageData(event.offsetX, event.offsetY, 1, 1).data[0],
-            g = ctx.getImageData(event.offsetX, event.offsetY, 1, 1).data[1],
-            b = ctx.getImageData(event.offsetX, event.offsetY, 1, 1).data[2];
-        previousColor = currentColor;
-        currentColor = rgbConvert(r, g, b);
-        if (previousColor === currentColor) {
-            return;
-        }
-        changeColors();
-    } else if (event.target === tool[1] || event.target === tool[1].children[0] || event.target === tool[1].children[1] || event === color) {
-        pickerActive = true;
-        activeTool = 'colorPicker';
-    }
-}
-
-function changeColors() {
-    if (previousColor === currentColor) {
-        return;
-    } else {
-        document.querySelector('.cur-color').style.backgroundColor = currentColor;
-        document.querySelector('.prev-color').style.backgroundColor = previousColor;
-    }
-}
-
-function changeCurrentColor(event) {
-    if (event.target === previous || event.target === previous.children[0] || event.target === previous.children[1]) {
-        let temp = currentColor;
-        currentColor = previousColor;
-        previousColor = temp;
-        changeColors();
-    }
-    if (event.target === red || event.target === red.children[0] || event.target === red.children[1]) {
-        previousColor = currentColor;
-		currentColor = redColor;
-        changeColors();
-    }
-    if (event.target === blue || event.target === blue.children[0] || event.target === blue.children[1]) {
-        previousColor = currentColor;
-        currentColor = blueColor;
-        changeColors();
-    }
-}
-
-function rgbConvert(r, g, b) {
-    r = r.toString(16);
-    g = g.toString(16);
-    b = b.toString(16);
-    if (r.length === 1) r = '0' + r;
-    if (g.length === 1) g = '0' + g;
-    if (b.length === 1) b = '0' + b;
-    return '#' + r + g + b;
-}
-
-function save() {
-    let arr = [];
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            let r = ctx.getImageData(scale * j + 1, scale * i + 1, 1, 1).data[0],
-                g = ctx.getImageData(scale * j + 1, scale * i + 1, 1, 1).data[1],
-                b = ctx.getImageData(scale * j + 1, scale * i + 1, 1, 1).data[2];
-            arr.push(rgbConvert(r, g, b));
-        }
-    }
-    localStorage.setItem('fieldStatus', arr);
-}
-
-function selectTool(event) {
-    for (let i = 0; i < tool.length; i++) {
-        tool[i].classList.remove("active-tool");
-        if (event.target === tool[i] || event.target === tool[i].children[0] || event.target === tool[i].children[1]) {
-            tool[i].classList.add("active-tool");
-            activeTool = tools[i];
-            if (activeTool !== 'colorPicker') {
-                pickerActive = false;
-            }
-        }
-    }
-}
-
-function selectCurrentColor() {
-    previousColor = currentColor;
-    currentColor = color.value;
-    changeColors();
-}
-
-function binds(event) {
-	switch(event.keyCode) {
-	case pencilCode:
-		for (let i = 0; i < tool.length; i++) {
-			tool[i].classList.remove('active-tool');
-		}
-        pencil.classList.add('active-tool');
-        activeTool = 'pencil';
-		pickerActive = false;
-		break;
-	case bucketCode:
-		for (let i = 0; i < tool.length; i++) {
-			tool[i].classList.remove('active-tool');
-		}
-        bucket.classList.add('active-tool');
-        activeTool = 'bucket';
-		pickerActive = false;
-		break;
-	case chooseCode:
-		for (let i = 0; i < tool.length; i++) {
-			tool[i].classList.remove('active-tool');
-		}
-        choose.classList.add('active-tool');
-        activeTool = 'colorPicker';
-		pickerActive = true;
-		colorPicker(event);
-		break;
-	}
-}
